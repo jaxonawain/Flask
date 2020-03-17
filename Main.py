bccfa8d139b63c6253b2381cbed5c4af8959fe37
Jaxonawain <jaxonawain@gmail.com>
2020-03-17 16:38:03 -0600

    started through authentication

--- Main.py
+++ Main.py
@@ -2,9 +2,9 @@ import Session, uuid, datetime, yaml
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
-# Validate against IP, need to include functionality to pair with username once authentication in place
-def validate():
-    print(request.remote_addr)
+
+def validate_session():
+    # Validate against IP, need to include functionality to pair with username once authentication in place
     headers = {'ip': request.remote_addr,
                'agent': request.headers.get('User-Agent')}
     sessionObj = Session.Session(headers)
@@ -13,32 +13,58 @@ def validate():
     else:
         return False
 
-# Create session in session table. Need to call with authentication script. Need to automatically remove from table too.
-def create_session():
-    # Gather headers
-    headers = {'ip': request.remote_addr,
-               'agent': request.headers.get('User-Agent'),
-               'timestamp': datetime.datetime.now(),
-               'guid': str(uuid.uuid4())}
-
-    sessionObj = Session.Session(headers)
-    return sessionObj.validate_session()
-
 
 @app.route('/')
 def home():
-    if validate():
+    if validate_session():
         return render_template('home.html')
     else:
         return render_template('login.html')
 
 
-@app.route('/login', methods=["POST"])
-def login():
-    return render_template('home.html')
+@app.route('/login', methods=["POST", "GET"])
+def login_page():
+    return render_template('login.html')
 
 
-#@app.route('.protein_tracker'):
+@app.route('/auth', methods=["POST"])
+def auth():
+    headers = {'username': request.form['username'],
+               'secret': request.form['secret']}
+    sessionObj = Session.Session(headers)
+    if sessionObj.validate_login():
+        return render_template('home.html')
+    else:
+        return render_template('login.html')
+
+
+@app.route('/register')
+def register_page():
+    return render_template('register.html')
+
+
+@app.route('/account_creation', methods=['POST'])
+def create_account():
+    headers = {'username': request.form['username'],
+               'secret': request.form['secret'],
+               'first_name': request.form['first_name'],
+               'last_name': request.form['last_name']}
+    sessionObj = Session.Session(headers)
+    sessionObj.create_user()
+    return render_template('login.html')
+
+
+@app.route('/session_creation')
+def create_session():
+    # Create session in session table. Need to call with authentication script. Need to automatically remove from
+    # table too.
+    headers = {'ip': request.remote_addr,
+               'agent': request.headers.get('User-Agent'),
+               'timestamp': datetime.datetime.now(),
+               'guid': str(uuid.uuid4())}
+    sessionObj = Session.Session(headers)
+    return sessionObj.start_session()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
--- Session.py
+++ Session.py
@@ -3,25 +3,45 @@ import mysql.connector, yaml
 
 class Session:
     def __init__(self, request_headers):
+        # Load config file into memory and set database parameters.
         with open('conf.yml', 'r') as conf_file:
             loaded_conf = yaml.load(conf_file, Loader=yaml.FullLoader)
-
-        db_host = loaded_conf.get('host')
-        user = loaded_conf.get('db_user')
-        secret = loaded_conf.get('db_secret')
-        db_name = 'flask_access_log'
         self.request_headers = request_headers
-        self.db_conn = mysql.connector.connect(host=db_host, user=user, passwd=secret, database=db_name)
+        self.db_conn = mysql.connector.connect(host=loaded_conf.get('host'), user=loaded_conf.get('db_user'),
+                                               passwd=loaded_conf.get('db_secret'), database='flask')
         self.db_cursor = self.db_conn.cursor()
 
     def start_session(self):
+        # Insert session data into session table
         query = "INSERT INTO sessions(access_date, ip_address, id, user_agent) VALUES (%s, %s, %s, %s);"
-        values = [self.request_headers['timestamp'], self.request_headers['ip'], self.request_headers['guid'], self.request_headers['agent']]
+        values = [self.request_headers['timestamp'], self.request_headers['ip'], self.request_headers['guid'],
+                  self.request_headers['agent']]
         self.db_cursor.execute(query, values)
         self.db_conn.commit()
         return self.request_headers
 
+    def create_user(self):
+        query = "INSERT INTO user_db(user_id, secret, first_name, last_name) VALUES (%s, %s, %s, %s);"
+        values = [self.request_headers['username'], self.request_headers['secret'], self.request_headers['first_name'],
+                  self.request_headers['last_name']]
+        self.db_cursor.execute(query, values)
+        self.db_conn.commit()
+        return True
+
+    def validate_login(self):
+        query = "SELECT user_id FROM user_db WHERE user_id=%s AND secret=%s"
+        values = [self.request_headers['username'], self.request_headers['secret']]
+        self.db_cursor.execute(query, values)
+        result = str(self.db_cursor.fetchall())
+        if self.request_headers["username"] in result:
+            return True
+        else:
+            print(result)
+            print(self.request_headers['username'])
+            return False
+
     def validate_session(self):
+        # Validate that there is a current session for user/machine
         query = """select ip_address from sessions where ip_address=%s;"""
         values = [self.request_headers['ip']]
         self.db_cursor.execute(query, values)
@@ -30,10 +50,6 @@ class Session:
             return str(results)
         else:
             return False
-
-        #create a validate login page
-
-
 
     def __del__(self):
         self.db_cursor.close()
